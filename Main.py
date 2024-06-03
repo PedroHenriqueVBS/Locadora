5d2aa1a77c2659008ecd430f12947646be1e46a8
Pedro Henrique <4ndroidplay.games@gmail.com>
2024-06-02 22:25:16 -0300

    AGORA EH UMA OUVIDORIA

--- Main.py
+++ Main.py
@@ -1,119 +1,122 @@
 from operacoesbd import *
 
 opcao = 0
-connection = criarConexao('127.0.0.1', 'root', '0000', 'locadora')
+connection = criarConexao('127.0.0.1', 'root', '0000', 'ouvidoria')
 while opcao != 7:
     print("""
-    +-------------------------------+
-    |         MENU PRINCIPAL        |
-    +-------------------------------+
-    | 1: Listar filmes              |
-    | 2: Listar por categoria       |
-    | 3: Pesquisar por nome         |
-    | 4: Adicionar filme            |
-    | 5: Excluir filme              |
-    | 6: Alterar filme              |
-    | 7: Sair                       |
-    +-------------------------------+
+    +---------------------------------------+
+    |             MENU PRINCIPAL            |
+    +---------------------------------------+
+    | 1: Listar Manifestações               |
+    | 2: Listar por categoria               |
+    | 3: Criar Manifestação                 |
+    | 4: Exibir quantidade de manifestações |
+    | 5: Pesquisar manifestação por código  |
+    | 6: Excluir Manifestação pelo Código   |                    
+    | 7: Sair do Sistema                    |
+    +---------------------------------------+
     """)
     opcao = int(input('Qual opção deseja? '))
 
     if opcao == 1:
-        # Imprimir todos os filmes do BD
-        busca = 'select Nome, Ano, Diretor, Categoria from filmes'
-        filmes = listarBancoDados(connection, busca)
+        # Imprimir todas as manifestacoes do BD
+        busca = 'select * from manifestacoes'
+        manifestacoes = listarBancoDados(connection, busca)
 
-        if len(filmes) == 0:
-            print('Nenhum filme disponível!')
+        if manifestacoes is None or len(manifestacoes) == 0:
+            print('Nenhuma manifestação encontrada!')
         else:
             # f-string para deixar com aparência de tabela
-            print(f"{'Nome':<30} {'Ano':<5} {'Diretor':<20} {'Categoria':<15}")
+            print(f"{'Codigo|':<6} {'Descricao':<50} {'|Categoria':<15}")
             print("=" * 70)
-            for filme in filmes:
-                nome, ano, diretor, categoria = filme
-                print(f"{nome:<30} {ano:<5} {diretor:<20} {categoria:<15}")
+            for manifestacao in manifestacoes:
+                Codigo, Descricao, Categoria = manifestacao
+                print(f"{Codigo:<6} {Descricao:<52} {Categoria:<15}")
 
     elif opcao == 2:
         # Listar por categoria
         # Sempre usando PlaceHolders para evitar SQL injection
-        tipo = input('Informe a categoria do filme: ').capitalize()
-        sql = 'SELECT Nome, Ano, Diretor, Categoria FROM filmes WHERE Categoria = %s'
-        filmes = listarBancoDados(connection, sql, (tipo,))
+        tipo = input('Informe a categoria da manifestação: ').capitalize()
+        sql = 'SELECT * FROM manifestacoes WHERE Categoria = %s'
+        manifestacoes = listarBancoDados(connection, sql, (tipo,))
 
-        if len(filmes) == 0:
-            print('Nenhum filme nessa categoria!')
+        if len(manifestacoes) == 0:
+            print('Nenhuma manifestação encontrada nessa categoria!')
         else:
-            print(f"{'Nome':<30} {'Ano':<5} {'Diretor':<20} {'Categoria':<15}")
+            # f-string para deixar com aparência de tabela
+            print(f"{'Codigo|':<6} {'Descricao':<50} {'|Categoria':<15}")
             print("=" * 70)
-            for filme in filmes:
-                nome, ano, diretor, categoria = filme
-                print(f"{nome:<30} {ano:<5} {diretor:<20} {categoria:<15}")
-
-    elif opcao == 3:
-        # Pesquisar por nome, passando por parâmentro sempre
-        nomeBusca = input('Informe o nome do filme: ')
-        sql = 'SELECT * FROM filmes WHERE Nome LIKE %s'
-        filmes = listarBancoDados(connection, sql, (nomeBusca,))
-
-        if len(filmes) == 0:
-            print('Nenhum filme encontrado!')
-        else:
-            print(f"{'Nome':<30} {'Ano':<5} {'Diretor':<20} {'Categoria':<15}")
-            print("=" * 70)
-            for filme in filmes:
-                nome, ano, diretor, categoria = filme
-                print(f"{nome:<30} {ano:<5} {diretor:<20} {categoria:<15}")
+            for manifestacao in manifestacoes:
+                Codigo, Descricao, Categoria = manifestacao
+                print(f"{Codigo:<6} {Descricao:<52} {Categoria:<15}")
 
     elif opcao == 4:
-        # Adicionar filme
-        nome = input('Informe o nome do filme: ').capitalize()
-        ano = int(input('Informe o ano do filme: '))
-        diretor = input('Informe o diretor do filme: ').capitalize()
-        categoria = input('Informe a categoria do filme: ').capitalize()
-        # Após capturar os dados do filme agrupa-os em uma tupla para inserir no BD
+        # Comando que seleciona todas as colunas e conta a quantidade de linhas
+        sql = 'SELECT COUNT(*) FROM `ouvidoria`.`manifestacoes`;'
+        qtdManifestacoes = listarBancoDados(connection, sql, )
+        # variavel para acessar a primeira posicao dentro da tupla
+        numTotal = str(qtdManifestacoes[0][0])
+        if numTotal == 0:
+            print('Nenhuma manifestação encontrada!')
+        else:
+            print('Existem um total de ' + numTotal + ' manifestações cadastradas!')
+
+
+    elif opcao == 3:
+        # Adicionar manifestacao
+        categoria = input('Informe a categoria da manifestação: (Crítica, Elogio, Sugestão) ').capitalize()
+
+        descricoesValidas = ['Crítica', 'Elogio', 'Sugestão']
+        #verificador para parametrizar as categorias
+        if categoria not in descricoesValidas:
+            print('Categoria inválida!')
+
+        else:
+            descricao = input('Descreva em seguida sua manifestação: ').capitalize()
+        # Após capturar os dados da manifestacao agrupa-os em uma tupla para inserir no BD
         # Respeitando a ordem pra funcionar nos placeholders
-
-        dados = (nome, ano, diretor, categoria)
-        sql = 'INSERT INTO filmes (Nome, Ano, Diretor, Categoria) VALUES (%s, %s, %s, %s)'
-        insertNoBancoDados(connection, sql, dados)
-
-    elif opcao == 5:
-        # Excluir filme
-        nomeFilme = input('Informe o nome do filme que você gostaria de excluir: ').capitalize()
-        sql = 'DELETE FROM filmes WHERE Nome = %s'
-
-        excluirBancoDados(connection, sql, (nomeFilme,))
+            dados = (categoria, descricao)
+            sql = 'INSERT INTO manifestacoes (Categoria, Descricao) VALUES (%s, %s)'
+            insertNoBancoDados(connection, sql, dados)
+            print('Manifestação adicionada com sucesso!')
 
     elif opcao == 6:
-        # Alterar filme
-        # Pergunta o nome e depois mostra as informações do filme
-        altNomeFilme = input('Digite o nome do filme que você gostaria de alterar: ').capitalize()
-        sqlNome = 'SELECT Nome, Ano, Diretor, Categoria FROM filmes WHERE Nome LIKE %s'
-        filmes = listarBancoDados(connection, sqlNome, (altNomeFilme,))
+        # Excluir manifestação
+        codManifestacao = input('Informe o código da manifestação que você gostaria de excluir: ').capitalize()
+        busca = 'SELECT Codigo FROM manifestacoes'
+        manifestacoes = listarBancoDados(connection, busca)
 
-        if len(filmes) == 0: #verificador
-            print('Filme não encontrado!')
+        # verificacao para ver se existe uma manifestacao correspondente ao codigo
+        if manifestacoes is None or len(manifestacoes) == 0:
+            print('Nenhuma manifestação encontrada!')
         else:
-            print(f"{'Nome':<30} {'Ano':<5} {'Diretor':<20} {'Categoria':<15}")
-            print("=" * 70)
-            for filme in filmes:
-                nome, ano, diretor, categoria = filme
-                print(f"{nome:<30} {ano:<5} {diretor:<20} {categoria:<15}")
 
-            # Pergunta qual informação o usuário quer alterar
-            altFilme = input('O que você gostaria de alterar? ').capitalize()
-            #verificador para que o usuário insira apenas colunas válidas
-            colunas_validas = ['Nome', 'Ano', 'Diretor', 'Categoria']
-            if altFilme not in colunas_validas:
-                print("Coluna inválida.")
-            else:
-                # Solicita o novo valor para o campo selecionado
-                # apenas a coluna está sendo passada diretamente na consulta pois não é possivel usar PlaceHolder
-                # Atualiza e exibe mensagem de sucesso
-                altThis = input(f'Qual é o novo valor para {altFilme}? ')
-                sql = f'UPDATE filmes SET {altFilme} = %s WHERE Nome = %s'
-                atualizarBancoDados(connection, sql, (altThis, altNomeFilme))
-                print(f"Filme '{altNomeFilme}' atualizado com sucesso.")
+            # Extrai os codigos de manifestacoes da lista de tuplas e converter para strings
+            codigosManifestacoes = [str(manifestacao[0]) for manifestacao in manifestacoes]
+
+        if codManifestacao not in codigosManifestacoes:
+                print('Manifestação não encontrada!')
+        else:
+            sql = 'DELETE FROM manifestacoes WHERE Codigo = %s'
+
+            excluirBancoDados(connection, sql, (codManifestacao,))
+            print('Manifestação excluida com sucesso!')
+
+    elif opcao == 5:
+        # Captura o codigo e passa na busca como parametro usando placeholders
+        codigo = input('Informe o codigo da manifestação: ').capitalize()
+        sql = 'SELECT * FROM manifestacoes WHERE Codigo = %s'
+        manifestacoes = listarBancoDados(connection, sql, (codigo,))
+
+        if len(manifestacoes) == 0:
+            print('Nenhuma manifestação encontrada com esse codigo!')
+        else:
+            # f-string para deixar com aparencia de tabela
+            print(f"{'Codigo|':<6} {'Descricao':<50} {'|Categoria':<15}")
+            print("=" * 70)
+            for manifestacao in manifestacoes:
+                Codigo, Descricao, Categoria = manifestacao
+                print(f"{Codigo:<6} {Descricao:<52} {Categoria:<15}")
 
     elif opcao == 7:
         encerrarBancoDados(connection)
